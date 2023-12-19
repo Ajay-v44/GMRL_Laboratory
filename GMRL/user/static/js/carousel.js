725af9f00954f90046f02124d48f58f1e08c9be8
Ajay v <vajay4834@gmail.com>
2023-12-19 22:21:43 +0530

    hero section added

--- GMRL/user/static/js/carousel.js
+++ GMRL/user/static/js/carousel.js
@@ -1,14 +1,33 @@
-var myIndex = 0;
-carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 3000); // Change image every 2 seconds
-}
+    const carouselData = [
+        {
+            title: "Unlocking Vital: Insights Your Blood, Your Lab",
+            text: "Unlocking Vital Insights: Where Your Blood Holds the Key to Your Health – Welcome to Your Trusted Laboratory..",
+        },
+        {
+            title: "Your Journey to Wellness: Precision Diagnostics, Compassionate Care.",
+            text: "Experience excellence in healthcare at our state-of-the-art lab,where precision meets compassion for your well-being.",
+        }
+        // Add more data as needed
+    ];
+
+    let currentIndex = 0;
+    const titleElement = document.getElementById("carouselTitle");
+    const textElement = document.getElementById("carouselText");
+    const slides = document.querySelectorAll(".mySlides");
+
+    function showSlide(index) {
+        titleElement.textContent = carouselData[index].title;
+        textElement.textContent = carouselData[index].text;
+        slides.forEach(slide => slide.style.display = "none");
+        slides[index].style.display = "block";
+    }
+
+    function carousel() {
+        showSlide(currentIndex);
+        currentIndex = (currentIndex + 1) % carouselData.length;
+        setTimeout(carousel, 3000); // Change image and content every 3 seconds
+    }
+
+    // Initial call to start the carousel
+    carousel();
