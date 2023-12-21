7b0ea2f6a55466f6c93410007db9a4cc6d6562d2
Ajay v <vajay4834@gmail.com>
2023-12-21 20:24:05 +0530

    initial pages completed

--- GMRL/user/urls.py
+++ GMRL/user/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('/aboutus',aboutus,name='aboutus'),
     path('/tests',testpage,name='tests'),
     path('/packages',packages,name='packages'),
-    path('/blogs',blogs,name='blogs')
+    path('/blogs',blogs,name='blogs'),
+    path('/gallery',gallery,name='gallery'),
+    path('/branches',branches,name='branches'),
+    path('/contactus',contact_us,name='contactus')
 ]
--- GMRL/user/views.py
+++ GMRL/user/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+
 def aboutus(request):
-    return render(request,'aboutus.html')
+    return render(request, 'aboutus.html')
+
+
 def testpage(request):
-    return render(request,'tests.html')
+    return render(request, 'tests.html')
+
+
 def packages(request):
-    return render(request,'packages.html')
+    return render(request, 'packages.html')
+
+
 def blogs(request):
-    return render(request,'blogs.html')
+    return render(request, 'blogs.html')
+
+
+def gallery(request):
+
+    return render(request, 'gallery.html')
+
+
+def branches(request):
+    return render(request, 'branches.html')
+
+
+def contact_us(request):
+    return render(request, 'contactus.html')
